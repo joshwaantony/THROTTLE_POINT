d54deaa65a36b8a96d53f4c57f4055b7382d5e86
Apple <apple@Apples-MacBook-Pro-2.local>
2025-06-25 10:36:24 +0530

    body style

--- src/component/Section4/Bodystyle.js
+++ src/component/Section4/Bodystyle.js
@@ -2,51 +2,51 @@ import React from "react";
 
 function Bodystyle() {
   return <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 border border-gray-300 bg-white cursor-pointer">
-    <div className="flex flex-col justify-center items-center border-r border-gray-300 border-b py-5 sm:py-10 gap-2 ">
+    <div className="flex flex-col justify-center items-center border-1 border-gray-300 border-b py-5 sm:py-10 gap-2 ">
       <img src="/sports.svg " className=" w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700   ">Sports Bike</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 border-b gap-2">
+        <div className="flex flex-col justify-center items-center border-1 border-gray-300 py-5 sm:py-10 border-b gap-2">
       <img src="/scooters.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700 ">Scooters</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 border-b py-5 sm:py-10 gap-2">
+        <div className="flex flex-col justify-center items-center  border-gray-300 border-1 py-5 sm:py-10 gap-2">
       <img src="/cruiser.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black  hover:text-green-700 ">cruiser</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 border-b gap-2">
+        <div className="flex flex-col justify-center items-center  border-gray-300 py-5 sm:py-10 border-1 gap-2">
       <img src="/commuter.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black  hover:text-green-700 ">Commuter</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 border-b py-5 sm:py-10 gap-2">
+        <div className="flex flex-col justify-center items-center  border-gray-300 border-1 py-5 sm:py-10 gap-2">
       <img src="/super-bikes.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700 ">Super Bikes</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 border-b py-5 sm:py-10 gap-2">
+        <div className="flex flex-col justify-center items-center  border-gray-300 border-1 py-5 sm:py-10 gap-2">
       <img src="/cafe-racer.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black  hover:text-green-700 ">Cafe Racer</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 border-b  py-5 sm:py-10 gap-3">
+        <div className="flex flex-col justify-center items-center  border-gray-300 border-1 py-5 sm:py-10 gap-3">
       <img src="/street-bikes.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700  ">Street Bikes</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 gap-2">
+        <div className="flex flex-col justify-center items-center border-1 border-gray-300 py-5 sm:py-10 gap-2">
       <img src="/scrambler.svg" className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700 ">Scrambler</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 gap-1">
+        <div className="flex flex-col justify-center items-center border-1 border-gray-300 py-5 sm:py-10 gap-1">
       <img src="/adventure.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700  ">Adventure</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 gap-4">
+        <div className="flex flex-col justify-center items-center border-1 border-gray-300 py-5 sm:py-10 gap-4">
       <img src="/moped.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black  hover:text-green-700 ">Moped</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 gap-2">
+        <div className="flex flex-col justify-center items-center border-1 border-gray-300 py-5 sm:py-10 gap-2">
       <img src="/tourer.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black  hover:text-green-700 ">Tourer</p>
     </div >
-        <div className="flex flex-col justify-center items-center border-r border-gray-300 py-5 sm:py-10 gap-2">
+        <div className="flex flex-col justify-center items-center border-1 border-gray-300 py-5 sm:py-10 gap-2">
       <img src="/maxi-scooters.svg " className="w-[60px] sm:w-[100px] hover:scale-110" alt="" />
       <p className="text-black hover:text-green-700  ">Maxi Scooters</p>
     </div >
